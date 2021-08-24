6196cf0c2a8b8e1ee5f183ba3f184f5b2f7f9289
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-08-24 16:10:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const searchInput = document.getElementById("search");
+const rows = document.querySelectorAll("tbody tr");
+console.log(rows);
+
+
+searchInput.addEventListener("keyup", function (event) {
+    const q = event.target.value.toLowerCase();
+    rows.forEach((row) => {
+        row.querySelector("td").textContent.toLowerCase().startsWith(q)
+        ? (row.style.display = "table-row")
+        : (row.style.display = "none");
+    });
+});
