f1d3b8e450486652437158427f14d48f8bf82938
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-08-26 12:08:04 +0530

    change the data from HTML to json

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
-const searchInput = document.getElementById("search");
-const rows = document.querySelectorAll("tbody tr");
-console.log(rows);
+
+$(document).ready(function () {
+  
+    // FETCHING DATA FROM JSON FILE
+    $.getJSON("data.json", 
+            function (data) {
+        var student = '';
+
+        // ITERATING THROUGH OBJECTS
+        $.each(data, function (key, value) {
+
+            //CONSTRUCTION OF ROWS HAVING
+            // DATA FROM JSON OBJECT
+            student += '<tr>';
+            student += '<td>' + 
+                value.District + '</td>';
+
+            student += '<td>' + 
+                value.Name + '</td>';
+
+            student += '<td>' + 
+                value.Address + '</td>';
+
+            student += '<td>' + 
+                value.Phone + '</td>';
+
+            student += '<td>' + 
+                value.WhatsApp + '</td>';
+
+            student += '<td>' + 
+                value.Viber + '</td>';
+
+            student += '<td>' + 
+                value.MoH + '</td>';
+
+            student += '<td>' + 
+                value.FIELD8 + '</td>';
 
 
-searchInput.addEventListener("keyup", function (event) {
-    const q = event.target.value.toLowerCase();
-    rows.forEach((row) => {
-        row.querySelector("td").textContent.toLowerCase().startsWith(q)
-        ? (row.style.display = "table-row")
-        : (row.style.display = "none");
+            student += '</tr>';
+        });
+          
+        //INSERTING ROWS INTO TABLE 
+        $('#table').append(student);
     });
 });
+
+
+
+// // jQuery
+// $.getData('/path/to/imported/data.js', function()
+// {
+//     pharmacyData.DataTable();
+// });
+
