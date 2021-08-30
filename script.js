43b3fc1e1b64a62ff743b8f43123d7b49c463c4d
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-08-30 13:33:39 +0530

    filter by district

--- script.js
+++ script.js
@@ -27,6 +27,40 @@ $.getJSON("data.json", function (data) {
 });
 
 
+$('#districtButtons button').click(function(event) {
+    var districtVal = this.textContent.toLowerCase()
+    console.log(districtVal)
+    filteredTableByDisctrict(districtVal)
+});
+
+
+
+function filteredTableByDisctrict(districtVal) {
+    var districts = [];
+    var District = "District";
+    $('#table tbody').html('');
+    for (var i=0 ; i < json.length ; i++)
+    {
+        if ((json[i][District].toLowerCase()).includes(districtVal)) {
+            filterDataForDistrict()
+        } 
+        function filterDataForDistrict() {
+            var district_data = '';
+            district_data += '<tr>';
+            district_data += '<td>' + json[i].District + '</td>';
+            district_data += '<td>' + json[i].Name + '</td>';
+            district_data += '<td>' + json[i].Address + '</td>';
+            district_data += '<td>' + json[i].Phone + '</td>';
+            district_data += '<td>' + json[i].MoH + '</td>';
+            district_data += '</tr>';
+
+            districts.push(district_data);
+            $('#table tbody').append(district_data);
+        }
+    }
+}
+
+
 function filteredTable() {
     var results = [];
     var District = "District";
